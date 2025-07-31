154d22b67bcd8dff767107c9cbb7e9b4b80240a5
Karlis Paegle <karlispaegle@gmail.com>
2025-07-31 12:20:10 +0300

    Update seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,40 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::factory()->create([
+        $testUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('password'),
             'is_admin' => true,
         ]);
 
+        $testUser2 = User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'test2@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
+
         $categories = Category::factory(5)->create();
 
-        Post::factory(10)->create()->each(function ($post) use ($categories) {
+        $commentedUsers = User::factory(5)->create();
+
+        $testUsers = [$testUser, $testUser2];
+
+        Post::factory(10)->create()->each(function ($post) use ($categories, $commentedUsers, $testUsers) {
+            $randomTestUser = $testUsers[array_rand($testUsers)];
+            $post->update(['user_id' => $randomTestUser->id]);
+
             $post->categories()->attach(
                 $categories->random(rand(1, 3))->pluck('id')->toArray()
             );
+
+            for ($i = 0; $i < rand(1, 5); $i++) {
+                $post->comments()->create([
+                    'content' => fake()->sentence(),
+                    'user_id' => $commentedUsers->random()->id,
+                ]);
+            }
         });
     }
 }
