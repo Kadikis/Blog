91848b9af56156fc17281f2ec28c08329941a6ba
Karlis Paegle <karlispaegle@gmail.com>
2025-08-01 09:59:55 +0300

    Fixed dashboard tests and removed demo tests.

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -21,6 +21,16 @@ class DashboardTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
 
+        //Non-admin users should not be able to access the dashboard
+        $response = $this->get('/admin/dashboard');
+        $response->assertStatus(500);
+    }
+
+    public function test_admin_users_can_access_the_dashboard()
+    {
+        $user = User::factory()->create(['is_admin' => true]);
+        $this->actingAs($user);
+
         $response = $this->get('/admin/dashboard');
         $response->assertStatus(200);
     }
